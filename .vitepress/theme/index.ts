4eccad255f066f26e28acabb18526789af977d58
Murasame <lsrl233@outlook.com>
2025-01-13 10:24:41 +0800

    更新歌曲

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -18,10 +18,19 @@ const playlist = [
     file: 'https://music.163.com/song/media/outer/url?id=473403174.mp3',
   },
   {
-    name: 'song2',
-    author: 'author2',
-    file: 'https://***.***.***/song2.mp3',
-    hide: true
+    name: 'キズナヒトツ(一丝羁绊)',
+    author: '山本希望',
+    file: 'https://music.163.com/song/media/outer/url?id=413077159.mp3',
+  },
+  {
+    name: 'ふたつの影(二人的映影)',
+    author: 'Famishin / 春風まゆき',
+    file: 'https://music.163.com/song/media/outer/url?id=473403185.mp3',
+  },
+  {
+    name: '我简直就是有地将臣！',
+    author: '凡思特 / 喜欢咩噶内烧酒',
+    file: 'https://music.163.com/song/media/outer/url?id=2118561956.mp3',
   },
 ]
 
